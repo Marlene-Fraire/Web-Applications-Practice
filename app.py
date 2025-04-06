f25ac1f9b2f06999fa469ad0d2397c9625dcc188
Marlene-Fraire <marlenefraire75@gmail.com>
2025-04-05 22:31:47 -0600

    Add config.toml and runtime.txt for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-ECHO is on.
+
 st.header('Lanzar una moneda') 
-ECHO is on.
-st.write('Esta aplicaci¢n aun no es funcional. En construcci¢n') 
+
+st.write('Esta aplicaci√≥n aun no es funcional. En construcci√≥n') 
